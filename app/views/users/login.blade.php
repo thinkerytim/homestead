34d910660e0310a8009a00df00b4470f7a1d209a
Tim Kramer <tim@thethinkery.net>
2014-11-18 22:18:42 -0800

    first new simplify admin work

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,21 +1,93 @@
-@extends('layouts.default')
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Think Closing</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-@section('content')
-{{ Form::open(array('url'=>'users/signin', 'class'=>'form-signin')) }}
-    <h2 class="form-signup-heading">Please Login</h2>
- 
-    <ul>
-        @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-    {{ Form::openGroup('email', 'Email') }}
-        {{ Form::text('email', null, array('placeholder'=>'Email Address')) }}
-    {{ Form::closeGroup() }}
-    {{ Form::openGroup('password', 'Password') }}
-        {{ Form::password('password', array('placeholder'=>'Password')) }}
-    {{ Form::closeGroup() }}
- 
-    {{ Form::submit('Login', array('class'=>'btn btn-large btn-primary btn-block'))}}
-{{ Form::close() }}
-@stop
+        <!-- Bootstrap core CSS -->
+        {{ HTML::style("assets/bootstrap/css/bootstrap.min.css") }}
+        
+        <!-- Font Awesome -->
+        {{ HTML::style("css/font-awesome.min.css") }}
+
+        <!-- ionicons -->
+        {{ HTML::style("css/ionicons.min.css") }}
+        
+        <!-- Simplify -->
+        {{ HTML::style("css/simplify.min.css") }}
+    
+    </head>
+
+    <body class="overflow-hidden light-background">
+        <div class="wrapper no-navigation preload">
+            <div class="sign-in-wrapper">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+                <div class="sign-in-inner">
+                    <div class="login-brand text-center">
+                        <i class="fa fa-database m-right-xs"></i> Think <strong class="text-skin">Closing</strong>
+                    </div>
+                    {{ Form::open(array('url'=>'users/signin', 'class'=>'form-signin')) }}
+                        <div class="form-group m-bottom-md">
+                            <input name="email" type="text" class="form-control" placeholder="Email Address">
+                        </div>
+                        <div class="form-group">
+                            <input name="password" type="password" class="form-control" placeholder="Password">
+                        </div>
+
+                        <div class="form-group">
+                            <div class="custom-checkbox">
+                                <input type="checkbox" id="chkRemember">
+                                <label for="chkRemember"></label>
+                            </div>
+                            Remember me
+                        </div>
+
+                        <div class="m-top-md p-top-sm">
+                            <a href="index.html" class="btn btn-success block">Sign in</a>
+                        </div>
+
+                        <div class="m-top-md p-top-sm">
+                            <div class="font-12 text-center m-bottom-xs">
+                                <a href="#" class="font-12">Forgot password ?</a>
+                            </div>
+                            <div class="font-12 text-center m-bottom-xs">Do not have an account?</div>
+                            <a href="{{ action('UsersController@getRegister', array()) }}" class="btn btn-default block">Create an accounts</a>
+                        </div>
+                    {{ Form::close() }}
+                </div><!-- ./sign-in-inner -->
+            </div><!-- ./sign-in-wrapper -->
+        </div><!-- /wrapper -->
+
+        <a href="" id="scroll-to-top" class="hidden-print"><i class="icon-chevron-up"></i></a>
+
+        <!-- Le javascript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        
+        <!-- Jquery -->
+        {{ HTML::script("assets/javascript/jquery-1.11.1.min.js") }}
+
+        <!-- Bootstrap -->
+        {{ HTML::script("assets/bootstrap/js/bootstrap.min.js") }}
+        
+        <!-- Slimscroll -->\
+        {{ HTML::script("assets/javascript/jquery.slimscroll.min.js") }}
+        
+        <!-- Popup Overlay -->
+        {{ HTML::script("assets/javascript/jquery.popupoverlay.min.js") }}
+
+        <!-- Modernizr -->
+        {{ HTML::script("assets/javascript/modernizr.min.js") }}
+        
+        <!-- Simplify -->
+        {{ HTML::script("assets/javascript/simplify/simplify.js") }}
+    
+    </body>
+</html>
